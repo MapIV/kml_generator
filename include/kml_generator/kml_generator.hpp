a5ab2bc6341d25437907ac5ef4d318bcb8efd01f
autoware <ryohei.sasaki@tier4.jp>
2022-04-14 08:52:45 +0900

    first commit

---
+++ include/kml_generator/kml_generator.hpp
@@ -0,0 +1,42 @@
+#ifndef KML_GENERATOR
+#define KML_GENERATOR
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include <sensor_msgs/NavSatFix.h>
+
+class KmlGenerator
+{
+public:
+  KmlGenerator();
+  KmlGenerator(std::string file_name);
+
+  void initKml();
+  bool addNavSatFixMsg(std::vector<sensor_msgs::NavSatFix>, std::string data_name);
+  bool addNavSatFixMsg(std::vector<sensor_msgs::NavSatFix>);
+  std::string NavSatFixMsgtoStr(std::vector<sensor_msgs::NavSatFix>);
+  bool addKmlHeader(std::string data_name);
+  bool addKmlBody(std::string data_name, std::string data_str);
+  bool outputKml();
+
+private:
+  // Variables
+  std::string file_name_ = "output.kml";
+  std::ofstream kml_file_ofs;
+  std::string header;
+  std::string body;
+  std::string footer;
+  int data_count = 0;
+
+  std::string color_list[8] 
+    = {"ff0000ff","ff00ff00","ffff0000","ffff55aa","ffffff00","ff7700ff","ff00aaff","ffffffff"};
+    //    Red        Green      Blue      Purple      Cyan      Magenta    Orange      White
+  
+
+};
+
+#endif
---
+++ src/kml_generator/kml_generator.cpp
@@ -0,0 +1,134 @@
+#include <kml_generator/kml_generator.hpp>
+
+KmlGenerator::KmlGenerator()
+{
+  kml_file_ofs.open(file_name_, std::ios::out);
+  std::cout << "Output file = " << file_name_ << std::endl;
+  initKml();
+}
+
+KmlGenerator::KmlGenerator(std::string file_name)
+{
+  file_name_ = file_name;
+  kml_file_ofs.open(file_name_, std::ios::out);
+  std::cout << "Output file = " << file_name_ << std::endl;
+  initKml();
+
+}
+
+void KmlGenerator::initKml()
+{
+   header =
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+      "<kml xmlns=\"http://earth.google.com/kml/2.2\">\n"
+      "<Document>\n"
+      "<name> eagleye </name>\n\n"
+
+      "<ScreenOverlay>\n"
+      "\t<name>Eagleye Logo</name>\n"
+      "\t<visibility>1</visibility>\n"
+      "\t<Icon>\n"
+      "\t\t<href>https://github.com/MapIV/eagleye/blob/master/docs/logo.png?raw=true</href>\n"
+      "\t</Icon>\n"
+      "\t<overlayXY x=\"-0.3\" y=\"-1\" xunits=\"fraction\" yunits=\"fraction\"/>\n"
+      "\t<screenXY x=\"0\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"/>\n"
+      "\t<size x=\"260.6\" y=\"56.0\" xunits=\"pixels\" yunits=\"pixels\"/>\n"
+      "</ScreenOverlay>\n\n";
+    
+   footer =
+      "</Document>\n"
+      "</kml>\n";
+}
+
+bool KmlGenerator::addKmlHeader(std::string data_name)
+{
+  std::string tmp_header;
+  std::string color_str = color_list[(data_count-1)%8];
+  tmp_header =
+      "<Style id=\" " + data_name + "\">\n"
+      "\t<LineStyle>\n"
+      "\t\t<color> " + color_str + " </color>\n"
+      "\t\t<width>5.00</width>\n"
+      "\t</LineStyle>\n"
+      "</Style>\n\n";
+  header +=tmp_header;
+
+  return true;
+}
+
+bool KmlGenerator::addKmlBody(std::string data_name, std::string data_str)
+{
+  std::string tmp_body;
+  tmp_body =
+      "\t<Placemark>\n"
+      "\t<name> " + data_name + " </name>\n"
+      "\t<visibility>1</visibility>\n"
+      "\t<description><![CDATA[]]></description>\n"
+      "\t<styleUrl># " + data_name + "</styleUrl>\n"
+      "\t<LineString>\n"
+      "\t\t<extrude>0</extrude>\n"
+      "\t\t<tessellate>1</tessellate>\n"
+      "\t\t<altitudeMode>clampToGround</altitudeMode>\n"
+      "\t\t<coordinates>\n"
+      + data_str +
+      "\t\t</coordinates>\n"
+      "\t</LineString>\n"
+      "\t</Placemark>\n\n";
+  body +=tmp_body;
+
+  return true;
+}
+
+std::string KmlGenerator::NavSatFixMsgtoStr(std::vector<sensor_msgs::NavSatFix> fix_msg)
+{
+  std::stringstream data_ss;
+  std::size_t data_length = std::distance(fix_msg.begin(), fix_msg.end());
+
+  double time_last = 0;
+  for (int i=0; i<data_length; i++)
+  {
+    if(fix_msg[i].header.stamp.toSec() != time_last
+      && fix_msg[i].header.stamp.toSec() - time_last > 0.2
+      && fix_msg[i].longitude != 0 && fix_msg[i].latitude != 0)
+    {
+      data_ss << std::setprecision(std::numeric_limits<double>::max_digits10) 
+        << fix_msg[i].longitude << "," 
+        << fix_msg[i].latitude << "," 
+        << fix_msg[i].altitude 
+        << std::endl;
+      time_last = fix_msg[i].header.stamp.toSec();
+    }
+  }
+
+  return data_ss.str();
+}
+
+bool KmlGenerator::addNavSatFixMsg(std::vector<sensor_msgs::NavSatFix> fix_msg)
+{
+  std::string data_str = NavSatFixMsgtoStr(fix_msg);
+  std::string data_name;
+
+  data_count++;
+  data_name = "DataNum " + std::to_string(data_count);
+
+  if (!addKmlHeader(data_name)) return false;
+  if (!addKmlBody(data_name,data_str)) return false;
+  return true;
+}
+
+bool KmlGenerator::addNavSatFixMsg(std::vector<sensor_msgs::NavSatFix> fix_msg, std::string data_name)
+{
+  std::string data_str = NavSatFixMsgtoStr(fix_msg);
+
+  data_count++;
+  if (!addKmlHeader(data_name)) return false;
+  if (!addKmlBody(data_name, data_str)) return false;
+  return true;
+}
+
+bool KmlGenerator::outputKml()
+{
+  kml_file_ofs << header << body <<  footer << std::endl;
+
+  return true;
+}
